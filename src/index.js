36484038900aacbc4f3ef0971d28c03b3b2a54da
PawelBelczyk <227667461+PawelBelczyk@users.noreply.github.com.>
2026-07-03 08:16:24 +0200

    3 buttons

--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
-console.log("Webpack działa 🚀");
 
 import createHomePage from "./home.js";
 import createMenuPage from "./menu.js";
 import createAboutPage from "./about.js";
 
+console.log("Webpack działa 🚀");
+
+
+
+const nav =document.createElement("nav");
+
+
+
+const menu = document.createElement("button");
+menu.textContent ="Menu";
+ 
+
+menu.addEventListener("click", () => {
+    createMenuPage()
+});
+
+
+const about = document.createElement("button");
+about.textContent ="About";
+ 
+about.addEventListener("click", () => {
+    createAboutPage()
+});
+
+const home = document.createElement("button");
+home.textContent="Home";
+ 
+home.addEventListener("click", () => {
+    createHomePage()
+});
+
+nav.appendChild(home);
+nav.appendChild(about);
+nav.appendChild(menu);
+
+document.body.appendChild(nav);
+
+
+
 createHomePage();
-createMenuPage();
-createAboutPage();
